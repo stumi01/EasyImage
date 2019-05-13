f9a5752bcfbfd283c3015af1075a354fd556e171
Bence Stumpf <stumiw@gmail.com>
2019-05-13 12:31:21 +0200

    Expose request codes

--- library/src/main/java/pl/aprilapps/easyphotopicker/Constants.kt
+++ library/src/main/java/pl/aprilapps/easyphotopicker/Constants.kt
@@ -11,7 +11,7 @@ package pl.aprilapps.easyphotopicker
 
 internal const val EASYIMAGE_LOG_TAG = "EasyImage"
 
-internal object RequestCodes {
+object RequestCodes {
     const val PICK_PICTURE_FROM_DOCUMENTS = 34961
     const val PICK_PICTURE_FROM_GALLERY = 34962
     const val PICK_PICTURE_FROM_CHOOSER = 34963
